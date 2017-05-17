7a486fd9d045a2c3e1308e1c5b6dc00c39275a73
PhreeMason <mr.omarmason@gmail.com>
2017-05-17 07:34:51 +0000

    Done.

--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
-
+    create_table :items do |t|
+      t.string :title
+      t.belongs_to :category
+      t.integer :inventory
+      t.decimal :price
+    end
   end
 end
